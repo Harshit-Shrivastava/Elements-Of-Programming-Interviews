aaed134283679b232de901233828dcad8669a8d7
Harshit Shrivastava <harshit.shrivastava14@gmail.com>
2019-10-05 19:55:51 -0700

    Solution to buy and sell stock twice

---
+++ 5. Arrays/5.7 Buy And Sell Stock Twice/Solution.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public static double buyAndSellStockTwice(List<Double> prices) {
+        double totalMaxProfit = 0.0;
+        List<Double> firstBuySellProfits = new ArrayList<>();
+        double minPriceSoFar = Double.MAX_VALUE;
+
+        // Forward phase, for each day, we record maximum profit we if we sell on that day
+        for (int i = 0; i < prices.size(); i++) {
+            minPriceSoFar = Math.min(minPriceSoFar, prices.get(i));
+            maxTotalProfit = Math.max(maxTotalProfit, prices.get(i) - minPriceSoFar);
+            firstBuySellProfits.add(maxTotalProfit);
+        }
+
+        // Backwards phase, for each day, find maximum profit if we make the second buy on that day
+        double maxPriceSoFar = Double.MIN_VALUE;
+        for (int i = prices.size() - 1; i > 0; i--) {
+            maxPriceSoFar = Math.max(maxPriceSoFar, prices.get(i));
+            maxTotalProfit = Math.max(maxTotalProfit, maxPriceSoFar - prices.get(i) + firstBuySellProfits.get(i-1));
+        }
+
+        return maxTotalProfit;
+    }
+}
